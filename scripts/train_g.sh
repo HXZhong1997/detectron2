d82f20c219bd9f9fcae293174942241cc1d0c0b8
hxzhong <hxzhong1997@163.com>
2022-01-20 17:49:19 +0800

    netG for faster-rcnn 198

--- scripts/train_g.sh
+++ scripts/train_g.sh
@@ -1,7 +1,7 @@
-python tools/plain_train_G.py --config-file configs/Net-G.yaml --config-det configs/COCO-Detection/faster_rcnn_R_50_FPN_1x_trained.yaml --num-gpus 4 OUTPUT_DIR output/netG_lr1e-2 
+python tools/plain_train_G.py --config-file configs/Net-G.yaml --config-det configs/COCO-Detection/faster_rcnn_R_50_FPN_1x_trained.yaml --num-gpus 4 OUTPUT_DIR output/netG_res_lr1e-2 NET_G.MASKTYPE 'residual'
 #rm output/netG_lr1e-2/model_00{0-4}*
 
-python tools/plain_train_G.py --config-file configs/Net-G.yaml --config-det configs/COCO-Detection/faster_rcnn_R_50_FPN_1x_trained.yaml --num-gpus 4 OUTPUT_DIR output/netG_lr5e-3  SOLVER.BASE_LR 0.005
+python tools/plain_train_G.py --config-file configs/Net-G.yaml --config-det configs/COCO-Detection/faster_rcnn_R_50_FPN_1x_trained.yaml --num-gpus 4 OUTPUT_DIR output/netG_res_lr5e-3  SOLVER.BASE_LR 0.005 NET_G.MASK_TYPE 'residual'
 
 #rm output/netG_lr1e-3/model_00{0-4}*
 
